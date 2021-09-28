0ba648930c81d5b01b0b1004f426dbc334be2c01
ProfessorWeb <84409940+ProfessorWeb@users.noreply.github.com>
2021-09-28 20:33:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,42 +5,42 @@ const links = document.querySelector('.navbar-nav');
 const gallary = document.querySelector('.photos');
 
 /*Gallary */
-const GallarySystem = function () {
-  const imagesUrl = new Map([
-    [
-      'img_1',
-      'https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
-    ],
-    [
-      'img2',
-      'https://images.unsplash.com/photo-1535850452425-140ee4a8dbae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1112&q=80',
-    ],
-    [
-      'img3',
-      'https://images.unsplash.com/photo-1552566626-52f8b828add9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
-    ],
-    [
-      'img4',
-      'https://images.unsplash.com/photo-1498654896293-37aacf113fd9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
-    ],
-    [
-      'img5',
-      'https://images.unsplash.com/photo-1428515613728-6b4607e44363?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
-    ],
-    [
-      'img6',
-      'https://images.unsplash.com/photo-1589472500123-b60f8455740b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80',
-    ],
-    [
-      'img7',
-      'https://images.unsplash.com/photo-1511795267602-0424436b99c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80',
-    ],
-    [
-      'img8',
-      'https://images.unsplash.com/photo-1553527922-767df645c5f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
-    ],
-  ]);
 
+const imagesUrl = new Map([
+  [
+    'img_1',
+    'https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
+  ],
+  [
+    'img2',
+    'https://images.unsplash.com/photo-1535850452425-140ee4a8dbae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1112&q=80',
+  ],
+  [
+    'img3',
+    'https://images.unsplash.com/photo-1552566626-52f8b828add9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
+  ],
+  [
+    'img4',
+    'https://images.unsplash.com/photo-1498654896293-37aacf113fd9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
+  ],
+  [
+    'img5',
+    'https://images.unsplash.com/photo-1428515613728-6b4607e44363?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
+  ],
+  [
+    'img6',
+    'https://images.unsplash.com/photo-1589472500123-b60f8455740b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80',
+  ],
+  [
+    'img7',
+    'https://images.unsplash.com/photo-1511795267602-0424436b99c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80',
+  ],
+  [
+    'img8',
+    'https://images.unsplash.com/photo-1553527922-767df645c5f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
+  ],
+]);
+const g = function () {
   imagesUrl.forEach(function (value, i) {
     const html = ` <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="${value}"  data-lightbox="photos"><img class="img-fluid" src="${value}"></a></div>`;
     gallary.insertAdjacentHTML('afterbegin', html);
@@ -52,10 +52,10 @@ links.addEventListener('click', function (e) {
   if (e.target.classList.contains('nav-link')) {
     const id = e.target.getAttribute('href');
     window.location.replace(id);
-    id === 'gallary.html' ? GallarySystem() : '';
-    console.log(id);
   }
 });
+
+window.location.href === 'http://127.0.0.1:8080/gallary.html' ? g() : '';
 
 $(document).ready(function () {
   $('body').css('display', 'none');
